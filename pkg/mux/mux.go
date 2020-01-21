ddb3d8a61ba64e9e43b8b2bfb6807782a84c4b82
Cody Collier <cody@telnet.org>
2020-01-20 19:29:45 -0600

    Init mux shell

--- pkg/mux/mux.go
+++ pkg/mux/mux.go
@@ -1,1 +1,18 @@
 package mux
+
+import (
+	pb "hum/proto"
+	"log"
+)
+
+// Initialize the core for a muxServer instance
+func initMux(mux *muxServer) error {
+
+	log.Printf("[muxd] Initializing Mux structures")
+
+	// Initialize the input / output channels
+	mux.mux_in = make(chan pb.Datum)
+	mux.mux_out = make(chan pb.Datum)
+
+	return nil
+}
